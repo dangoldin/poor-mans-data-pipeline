064b2a74e2adb3b0f6ecc98c2a53db7d27688d04
Dan Goldin <dangoldin@gmail.com>
2016-10-23 21:58:18 -0400

    Get it working on a directory for testing

--- parse_elb_log.py
+++ parse_elb_log.py
@@ -1,24 +1,48 @@
 #!/usr/bin/python
 
+import os
 import sys
 import io
 from operator import itemgetter
+from collections import Counter
 
-# TODOS: Parse ip, user agent
+# TODOs: Parse ip, user agent
+
+def parse_line(line):
+    dt, path = itemgetter(0,12)(line.split(' '))
+    dt = dt[:10] # Just the ymd
+    path = path.split('/')[-1] # Ignore the host
+    return (dt, path)
 
 def process_file(fn):
+    summary = Counter()
+
     with io.open(fn, 'r') as f:
         for line in f:
-            dt, path = itemgetter(0,12)(line.split(' '))
-            dt = dt[:10] # Just the ymd
-            path = path.split('/')[-1] # Ignore the host
-            print dt, path
+            summary[parse_line(line)] += 1
+
+    return summary
+
+def process_directory(d):
+    summary = Counter()
+
+    for f in os.listdir(d):
+        fn = os.path.join(d, f)
+        if os.path.isfile(fn):
+            summary += process_file(fn)
+
+    return summary
 
 if __name__ == '__main__':
     if len(sys.argv) < 2:
-        print 'Specify a file'
+        print 'Specify a file or directory'
         sys.exit(1)
 
     fn = sys.argv[1]
 
-    process_file(fn)
+    if os.path.isdir(fn):
+        out = process_directory(fn)
+    else:
+        out = process_file(fn)
+
+    print out
