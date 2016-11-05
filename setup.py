6bd25c08ade2860d427e27d57988125a0730d42e
Dan Goldin <dangoldin@gmail.com>
2016-11-04 22:33:29 -0400

    Added manifest and setups

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'pmdp',
+  packages = ['pmdp'], # this must be the same as the name above
+  version = '0.1',
+  description = 'A poor man\'s data pipeline',
+  author = 'Dan Goldin',
+  author_email = 'dangoldin@gmail.com',
+  url = 'https://github.com/dangoldin/poor-mans-data-pipeline', # use the URL to the github repo
+  download_url = 'https://github.com/dangoldin/poor-mans-data-pipeline/tarball/0.1', # I'll explain this in a second
+  keywords = ['data', 'data-pipeline'], # arbitrary keywords
+  classifiers = [],
+)
