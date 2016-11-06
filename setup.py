31d855662fd4beed7d5b2675c45a223eae8ff01e
Dan Goldin <dangoldin@gmail.com>
2016-11-05 22:16:21 -0400

    More cleanup

--- setup.py
+++ setup.py
@@ -1,13 +1,15 @@
+# Mostly from http://peterdowns.com/posts/first-time-with-pypi.html
+
 from distutils.core import setup
 setup(
   name = 'pmdp',
-  packages = ['pmdp'], # this must be the same as the name above
+  packages = ['pmdp'],
   version = '0.1',
   description = 'A poor man\'s data pipeline',
   author = 'Dan Goldin',
   author_email = 'dangoldin@gmail.com',
-  url = 'https://github.com/dangoldin/poor-mans-data-pipeline', # use the URL to the github repo
-  download_url = 'https://github.com/dangoldin/poor-mans-data-pipeline/tarball/0.1', # I'll explain this in a second
-  keywords = ['data', 'data-pipeline'], # arbitrary keywords
+  url = 'https://github.com/dangoldin/poor-mans-data-pipeline',
+  download_url = 'https://github.com/dangoldin/poor-mans-data-pipeline/tarball/0.2',
+  keywords = ['data', 'data-pipeline'],
   classifiers = [],
 )
